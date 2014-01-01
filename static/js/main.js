0e2262c31604525569a712019e4cef40479e611a
Simon Forman <forman.simon@gmail.com>
2013-12-31 17:54:44 -0800

    Allow for "rotating" messages.

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,13 @@
+var a_list = [ "hey", "poop", "gonzo" ];
+var index = 0;
+
 function hey() {
-  $("#heythere").html("Hello World!");
+  var message = a_list[index];
+  $("#heythere").html(message);
+  index = index + 1;
+  if (index >= a_list.length) {
+    index = 0;
+  }
 };
 
 $(document).ready(function(){
