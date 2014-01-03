cc0e2f600be09f08dd06951029f9083f3c118e72
Victor Simones <mr.victorsimones@gmail.com>
2014-01-02 21:15:26 -0800

    updated, Check it out :0)

--- site/static/js/main.js
+++ site/static/js/main.js
@@ -3,14 +3,17 @@ var index = 0;
 
 function hey() {
   $("#heythere").html("Hello World!");
-  var message a_list[index];
+  var message = a_list[index];
   $("#heythere").html(message);
   index = index + 1;
-  if (index >= a_list.lenght)
+  if (index >= a_list.length)
     index = 0;
   }
  };
- $(document).ready(function()
+ $(document).ready(function(){
+   $("#heythere").click(hey);
+ });
+ }
  
 
 
